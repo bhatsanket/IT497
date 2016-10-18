2da2fbee16f9f932b3daf2bbf8451752b959b090
bhatsanket <bhatsanket@gmail.com>
2016-10-18 17:52:34 -0500

    Extra credit

---
+++ PleaseGiveMeExtraPoints.r
@@ -0,0 +1,11 @@
+##By Sanket Bhat.
+#First number
+a <- 151
+#second number
+b <- 200
+#third number
+c <- 7
+#fourth number = difference of first and second number divided by third number.
+d <- (b-a)/c
+#display
+d
