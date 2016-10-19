afaa5e16f779d692b79de2f58838fad4f1dbd63f
bhatsanket <bhatsanket@gmail.com>
2016-10-19 10:58:27 -0500

    updated with quiz comments

--- PleaseGiveMeExtraPoints.r
+++ PleaseGiveMeExtraPoints.r
@@ -9,3 +9,14 @@ c <- 7
 d <- (b-a)/c
 #display
 d
+
+##On the quiz question
+##if you compare the summary for x and y, yes the data maybe the same from view perspective
+#but if we check the data type it changes...hence i don't think the content can be considered same.
+url3 <-"https://raw.githubusercontent.com/jrwolf/IT497/master/apps_count.csv"
+x <- repmis::source_data(url3)
+y <- read.csv(url3)
+str(x)
+str(y)
+summary(x)
+summary(y)
